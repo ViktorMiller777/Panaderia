18d9ebad9a0e47cbf6a6f62d7a96e2759fb56f81
TaniaCasta10 <castanedatanializeth@gmail.com>
2023-07-01 14:30:34 -0600

    Se agrego el boton de categoria

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,18 @@
         <center><li><RouterLink class="rutas" to="/login"><img class="foto" src="/src/img/perfil.png" alt=""></RouterLink></li></center><br>
         <center><li><RouterLink class="rutas" to="/almacen">Almacen</RouterLink></li></center><br>
         <center><li><RouterLink class="rutas" to="/sucursales">Sucursales</RouterLink></li></center><br>
-        <center><li><RouterLink class="rutas" to="/categorias">Categorias</RouterLink></li></center><br>
+        <div class="select">
+        <select name="format" id="format">
+            <option selected disabled>Categorias</option>
+            <option value="panimart">Panimart</option>
+            <option value="coberturasmym">Coberturas MyM</option>
+            <option value="materiasprimas">Materia Prima</option>
+            <option value="interventas">Interventas</option>
+            <option value="americandonuts">American Donuts</option>
+            <option value="pacasdepapel">Pacas de papel</option>
+            <option value="productosdelimpieza">Productos de limpieza</option>
+        </select>
+    </div>
         
       </ul>
   </div>
@@ -53,5 +64,51 @@ height: 35px;
   font-style: oblique;
   color:black
 }
+
+ select {
+    -webkit-appearance:none;
+    -moz-appearance:none;
+    -ms-appearance:none;
+    appearance:none;
+    outline:0;
+    box-shadow:none;
+    border:0!important;
+    background: #5c6664;
+    background-image: none;
+    flex: 1;
+    padding: 0 .5em;
+    color:black;
+    cursor:pointer;
+    font-size: 1em;
+    font-family: 'Open Sans', sans-serif;
+ }
+ select::-ms-expand {
+    display: none;
+ }
+ .select {
+    position: relative;
+    display: flex;
+    width: 16em;
+    height: 3em;
+    line-height: 3;
+    background: #5c6664;
+    overflow: hidden;
+    border-radius: .25em;
+ }
+ .select::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 0 1em;
+    background: #2b2e2e;
+    cursor:pointer;
+    pointer-events:none;
+    transition:.25s all ease;
+ }
+ .select:hover::after {
+    color:white;
+ }
+ 
 
 </style>
