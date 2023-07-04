701d553fdeb70a6e9a296a4a1baa5bd78636a35b
viktor <cabellordz15@gmail.com>
2023-07-04 11:50:02 -0600

    belleza de pan

--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -6,6 +6,9 @@
     <div>
         <h1>HELLO EVERYNYAM HOW ARE YOU, FINE THANK YOU :D</h1>
     </div>
+    <div>
+        <img src="/src/img/rico.jpg" alt="">
+    </div>
 </template>
 <style scoped>
 </style>
