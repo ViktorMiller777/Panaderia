1cd3a6873bee5310e2716479224ed6abc4875887
Jack_DLC327 <joseangelgarciadelacruz8@gmail.com>
2023-07-06 19:33:00 -0600

    3er intento0

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/loginadd',
+      path:'/login',
       name:'login',
       component:()=>import('../views/login.vue')
     },
